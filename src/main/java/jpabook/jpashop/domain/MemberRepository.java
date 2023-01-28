e21f7e55c6459aa3f6bc9e89731a16e84d279adf
enamu <guyus5594@gmail.com>
2023-01-28 20:03:05 +0900

    주문 검색 기능 개발, 테스트

--- src/main/java/jpabook/jpashop/domain/MemberRepository.java
+++ src/main/java/jpabook/jpashop/domain/MemberRepository.java
@@ -1,6 +1,6 @@
-package jpabook.jpashop.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
+//package jpabook.jpashop.domain;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface MemberRepository extends JpaRepository<Member, Long> {
+//}
